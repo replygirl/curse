cda7f725a02fe1427376b533eb4a7addf58f6cfa
replygirl <moon@moon.fyi>
2021-02-20 13:12:41 -0500

    grow

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,52 @@
-interface Handlers<T> {
-  arr: (x: T[]) => any
-  obj: (x: Record<any, T>) => any
-  prim: (x: T) => any
+export interface CurseHandlers {
+  arr: (x: any[], k?: string) => any
+  obj: (x: Record<any, any>, k?: string) => any
+  key: (x: string, k?: string) => any
+  val: (x: Exclude<any, any[] | object>, k?: string) => any
 }
 
-const _default = <T = any>(
+const curse = <T = any, R = undefined>(
   x: T,
   {
-    arr = (x: T[]) => x,
-    obj = (x: Record<any, T>) => x,
-    prim = (x: T) => x
-  }: Partial<Handlers<T>> = {}
-): typeof x extends any[]
-  ? ReturnType<typeof arr>
-  : typeof x extends NonNullable<object>
-  ? ReturnType<typeof obj>
-  : ReturnType<typeof prim> =>
+    arr = x => x,
+    obj = x => x,
+    key = x => x,
+    val = x => x
+  }: Partial<CurseHandlers> = {},
+  pk?: string
+): R extends undefined
+  ? typeof x extends any[]
+    ? ReturnType<typeof arr>
+    : typeof x extends NonNullable<object>
+    ? ReturnType<typeof obj>
+    : ReturnType<typeof val>
+  : R =>
   Array.isArray(x)
-    ? arr(x.map(y => _default(y, { arr, obj, prim })))
+    ? arr(
+        x.map(y => curse<typeof y>(y, { arr, obj, key, val }, pk)),
+        pk
+      )
     : typeof x === 'object' && x != null
     ? obj(
         Object.entries(x).reduce(
           (a, [k, v]) => ({
             ...a,
-            [k]: _default(v, { arr, obj, prim })
+            [key(k)]: curse<typeof v>(v, { arr, obj, key, val }, key(k))
           }),
           {}
-        )
+        ),
+        pk
       )
-    : prim(x)
+    : val(x, pk)
+
+const _default = <T = any, R = undefined>(
+  x: T,
+  {
+    arr = x => x,
+    obj = x => x,
+    key = x => x,
+    val = x => x
+  }: Partial<CurseHandlers> = {}
+) => curse<T, R>(x, { arr, obj, key, val })
 
 export default _default
